e78228b012cb32b4de84ad701078f814bc4e5ae8
riz211 <riz211@yahoo.com>
2025-01-11 03:26:50 -0600

    Update app.py

--- project-folder/app.py
+++ project-folder/app.py
@@ -277,7 +277,7 @@ if st.button("Export to Excel"):
 
                 # Add formulas to the relevant columns
                 worksheet.cell(row=row_index, column=10).value = f"=IF(I{row_index}<>\"\", ROUND(VLOOKUP(I{row_index}, ShippingLegend!A:C, 3, TRUE), 2), \"\")" # SHIPPING COST formula
-                worksheet.cell(row=row_index, column=11).value = f"=IF(AND(E{row_index}<>\"\", F{row_index}<>\"\", J{row_index}<>\"\"), "f"ROUND((E{row_index}+F{row_index}+J{row_index})*1.35,2) \"\")"  # RETAIL PRICE formula
+                worksheet.cell(row=row_index, column=11).value = (f"=IF(AND(E{row_index}<>\"\", F{row_index}<>\"\", J{row_index}<>\"\"), "f"ROUND((E{row_index}+F{row_index}+J{row_index})*1.35, 2), \"\")")  # RETAIL PRICE formula                
                 worksheet.cell(row=row_index, column=12).value = f"=K{row_index}"  # MIN PRICE formula
                 worksheet.cell(row=row_index, column=13).value = f"=IF(L{row_index}<>\"\", ROUND(L{row_index}*1.35, 2),\"\")"  # MAX PRICE formula
 
